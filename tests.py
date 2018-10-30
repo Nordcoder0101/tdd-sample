1eaecbda6d003e2c23871c5596ad84ea93b177eb
Greg <greg@Gregs-MacBook-Air.local>
2018-10-30 15:52:12 -0700

    2 tests written for isPalindrom

--- tests.py
+++ tests.py
@@ -1,14 +1,19 @@
 import unittest
 import reverseList
+import isPalindrome
 
 class reverseListTest(unittest.TestCase):
-  def testOne(self):
+  def reversedListTestOne(self):
       self.assertEqual(reverseList.reverseList([5,4,3,2,1]), [1,2,3,4,5])
 
-  def testTwo(self):
+  def reversedListTestTwo(self):
       self.assertEqual(reverseList.reverseList([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
 
-      
+  def isPalindromeTestOne(self):
+      self.assertTrue(isPalindrome.isPalindrome('racecar'), True)
+
+  def isPalindromeTestTwo(self):
+      self.assertFalse(isPalindrome.isPalindrome('burger'), False)
 
 if __name__ == '__main__':
     unittest.main()
